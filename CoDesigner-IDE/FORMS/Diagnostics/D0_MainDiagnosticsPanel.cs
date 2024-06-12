081d2551913f1e67cf57dcb890b901f883e03649
Paul Deian <deian2607@gmail.com>
2024-06-13 00:57:23 +0300

    #4 Add basic elements
    Add basic elements (folders, some forms) and a draft of the build checker GitHub Action, as well as a .gitignore file to ignore artifacts.

---
+++ CoDesigner-IDE/FORMS/Diagnostics/D0_MainDiagnosticsPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoDesigner_IDE.FORMS.Diagnostics
+{
+    public partial class D0_MainDiagnosticsPanel : Form
+    {
+        public D0_MainDiagnosticsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
